5fd7014585210abe12a698302484e2bb0daf387f
Starciad <davigamerlsf16@gmail.com>
2024-01-02 19:19:04 -0300

    feat: Add utility methods to the transform component.

--- src/DD/Components/Common/DTransformComponent.cs
+++ src/DD/Components/Common/DTransformComponent.cs
@@ -7,5 +7,38 @@ namespace DD.Components.Common
         internal Vector2 Position { get; set; }
         internal Vector2 Scale { get; set; }
         internal float Rotation { get; set; }
+
+        public override void Reset()
+        {
+            base.Reset();
+            this.Position = Vector2.Zero;
+            this.Scale = Vector2.One;
+            this.Rotation = 0f;
+        }
+
+        internal void SetPosition(Vector2 newPosition)
+        {
+            Position = newPosition;
+        }
+
+        internal void SetRotation(float angleInRadians)
+        {
+            Rotation = angleInRadians;
+        }
+
+        internal void MoveRelative(Vector2 offset)
+        {
+            Position += offset;
+        }
+
+        internal void Resize(Vector2 newScale)
+        {
+            Scale = newScale;
+        }
+
+        internal void Rotate(float angleInRadians)
+        {
+            Rotation += angleInRadians;
+        }
     }
 }
