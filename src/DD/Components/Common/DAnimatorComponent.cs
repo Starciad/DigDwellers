e3a184511ca1f55d3aa3c547d27ee64ba88b73ae
Starciad <davigamerlsf16@gmail.com>
2024-01-03 20:47:41 -0300

    feat: Add pause for animation component.

--- src/DD/Components/Common/DAnimatorComponent.cs
+++ src/DD/Components/Common/DAnimatorComponent.cs
@@ -1,12 +1,21 @@
 ﻿using DD.Animation;
+using DD.Animation.Enums;
 
 using Microsoft.Xna.Framework;
+
+using System.Collections.Generic;
 
 namespace DD.Components.Common
 {
     internal sealed class DAnimatorComponent : DComponent
     {
-        private readonly DAnimation animation = new();
+        internal string CurrentAnimationName => this.currentAnimationName;
+        internal DAnimation CurrentAnimation => this.currentAnimation;
+
+        private readonly Dictionary<string, DAnimation> animations = [];
+
+        private string currentAnimationName = string.Empty;
+        private DAnimation currentAnimation = null;
 
         private DDrawComponent _drawComponent;
 
@@ -14,8 +23,10 @@ namespace DD.Components.Common
         {
             base.Reset();
 
-            this.animation.Reset();
-            this.animation.ClearFrames();
+            this.animations.Clear();
+
+            this.currentAnimationName = string.Empty;
+            this.currentAnimation = null;
         }
         protected override void OnAwake()
         {
@@ -27,15 +38,78 @@ namespace DD.Components.Common
         {
             base.OnUpdate(gameTime);
 
-            if (this.animation == null)
+            if (string.IsNullOrEmpty(this.currentAnimationName))
             {
                 return;
             }
 
-            this._drawComponent.SetTexture(this.animation.Texture);
-            this._drawComponent.SetSourceRectangle(this.animation.SourceFrame);
+            this._drawComponent.SetTexture(this.currentAnimation.Texture);
+            this._drawComponent.SetSourceRectangle(this.currentAnimation.SourceFrame);
 
-            this.animation.Update();
+            this.currentAnimation.Update();
+        }
+
+        internal DAnimation AddAnimation(string name)
+        {
+            DAnimation anim = new();
+            this.animations.Add(name, anim);
+
+            return anim;
+        }
+        internal DAnimation GetAnimation(string name)
+        {
+            return animations[name];
+        }
+        internal void RemoveAnimation(string name)
+        {
+            _ = this.animations.Remove(name);
+        }
+
+        internal void Play(string name)
+        {
+            if (this.currentAnimationName.Equals(name))
+            {
+                return;
+            }
+
+            if (animations.TryGetValue(name, out DAnimation animation))
+            {
+                animation.Reset();
+                animation.SetMode(DAnimationMode.Forward);
+
+                this.currentAnimation = animation;
+                this.currentAnimationName = name;
+            }
+        }
+        internal void Stop()
+        {
+            if (string.IsNullOrEmpty(this.currentAnimationName))
+            {
+                return;
+            }
+
+            this.currentAnimation = null;
+            this.currentAnimationName = string.Empty;
+
+            this.currentAnimation.Reset();
+        }
+        internal void Pause()
+        {
+            if (string.IsNullOrEmpty(this.currentAnimationName))
+            {
+                return;
+            }
+
+            this.currentAnimation.Pause();
+        }
+        internal void Resume()
+        {
+            if (string.IsNullOrEmpty(this.currentAnimationName))
+            {
+                return;
+            }
+
+            this.currentAnimation.Resume();
         }
     }
 }
