97b6b99bc891d979859f7fd71513578229e19b1f
Starciad <davigamerlsf16@gmail.com>
2024-01-03 19:32:37 -0300

    feat: Add animation class in the corresponding component.

--- src/DD/Components/Common/DAnimatorComponent.cs
+++ src/DD/Components/Common/DAnimatorComponent.cs
@@ -1,7 +1,41 @@
-﻿namespace DD.Components.Common
+﻿using DD.Animation;
+
+using Microsoft.Xna.Framework;
+
+namespace DD.Components.Common
 {
     internal sealed class DAnimatorComponent : DComponent
     {
+        private readonly DAnimation animation = new();
 
+        private DDrawComponent _drawComponent;
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            this.animation.Reset();
+            this.animation.ClearFrames();
+        }
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            this.Entity.ComponentContainer.TryGetComponent(out _drawComponent);
+        }
+        protected override void OnUpdate(GameTime gameTime)
+        {
+            base.OnUpdate(gameTime);
+
+            if (this.animation == null)
+            {
+                return;
+            }
+
+            this._drawComponent.SetTexture(this.animation.Texture);
+            this._drawComponent.SetSourceRectangle(this.animation.SourceFrame);
+
+            this.animation.Update();
+        }
     }
 }
